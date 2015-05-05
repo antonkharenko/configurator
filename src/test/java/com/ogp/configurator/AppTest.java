a53e644e6aa5038fab8ee3cb471bf6b016986738
Anton Kharenko <a.kharenko@gmail.com>
2015-05-05 13:13:03 +0300

    Project stub

---
+++ src/test/java/com/ogp/configurator/AppTest.java
@@ -0,0 +1,13 @@
+package com.ogp.configurator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
