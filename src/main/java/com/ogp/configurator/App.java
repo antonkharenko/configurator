7e18bcfdfd12e145bfe404513c349527f1fac27b
Anton Kharenko <a.kharenko@gmail.com>
2015-05-05 13:48:15 +0300

    Some dependencies

--- src/main/java/com/ogp/configurator/App.java
+++ src/main/java/com/ogp/configurator/App.java
@@ -1,8 +1,16 @@
 package com.ogp.configurator;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String zookeeperConnectionString = "127.0.0.1:2181";
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+		CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+		client.start();
 	}
 }
